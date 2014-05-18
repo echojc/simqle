2cb6127c1484be138d019085a68e392e34874691
Jonathan Chow <me@echo.sh>
2014-05-18 21:01:37 +1200

    Proof of concept with indexed extractors.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -10,29 +10,39 @@ object A {
   implicit object intExtractor extends Extractor[Int] { def extract = 1 }
   implicit object stringExtractor extends Extractor[String] { def extract = "a" }
 
-  //trait NE[T] { def extract(i: Int): T }
-  //implicit object intNExtractor extends NE[Int] { def extract(i: Int) = i }
-  //implicit object stringNExtractor extends NE[String] { def extract(i: Int) = ('a' + i - 1).toString }
+  trait NE[T] { def extract(i: Int): T }
+  implicit object intNExtractor extends NE[Int] { def extract(i: Int) = i }
+  implicit object stringNExtractor extends NE[String] { def extract(i: Int) = ('a' + i - 1).toChar.toString }
+  implicit object doubleNExtractor extends NE[Double] { def extract(i: Int) = 1.0 * i }
 
-  implicit object hnilExtractor extends Extractor[HNil] { def extract = HNil }
-  trait HListExtractor[L <: HList] extends Extractor[L] {
+  import nat._
+  import ops.nat._
+  trait LowerPriority {
+    implicit def hlistExtractor[L <: HList](implicit hlnx: HListNExtractor[L, _2]): HListNExtractor[L, _2] = hlnx
+  }
+
+  trait HListNExtractor[L <: HList, N <: Nat] extends Extractor[L] {
     type H
     type T <: HList
   }
-  object HListExtractor {
-    def apply[L <: HList](implicit hlx: HListExtractor[L]): Aux[L, hlx.H, hlx.T] = hlx
-    type Aux[L <: HList, H0, T0 <: HList] = HListExtractor[L] { type H = H0; type T = T0 }
-    implicit def auxExtractor[H0, T0 <: HList](implicit hx: Extractor[H0], tx: Extractor[T0]): Aux[H0 :: T0, H0, T0] =
-      new HListExtractor[H0 :: T0] {
+  implicit def hnilNExtractor[N <: Nat] = new HListNExtractor[HNil, N] { def extract = HNil }
+  object HListNExtractor extends LowerPriority {
+    def apply[L <: HList, N <: Nat](implicit hlx: HListNExtractor[L, N]): Aux[L, hlx.H, hlx.T, N] = hlx
+
+    type Aux[L <: HList, H0, T0 <: HList, N <: Nat] = HListNExtractor[L, N] { type H = H0; type T = T0 }
+
+    implicit def auxExtractor[H0, T0 <: HList, N <: Nat](
+      implicit hx: NE[H0], tx: HListNExtractor[T0, Succ[N]], toInt: ToInt[N]): Aux[H0 :: T0, H0, T0, N] =
+      new HListNExtractor[H0 :: T0, N] {
         type H = H0
         type T = T0
-        def extract: H0 :: T0 = hx.extract :: tx.extract
+        def extract: H0 :: T0 = hx.extract(toInt()) :: tx.extract
       }
   }
 
   import ops.hlist.Tupler
   implicit def tupleExtractor[P, L <: HList](
-    implicit gen: Generic.Aux[P, L], hx: HListExtractor[L], tp: Tupler.Aux[L, P]) =
+    implicit gen: Generic.Aux[P, L], hx: HListNExtractor[L, _1], tp: Tupler.Aux[L, P]) =
     new Extractor[P] {
       def extract: P = tp(hx.extract)
     }
@@ -41,10 +51,10 @@ object A {
 
 object Main extends App {
   import A._
-  import A.HListExtractor._
-  assert(implicitly[Extractor[Int :: String :: HNil]].extract == 1 :: "a" :: HNil)
-  assert(implicitly[Extractor[(Int, String)]].extract == (1, "a"))
-  //implicitly[Q[(Int, String)]]
+  import A.HListNExtractor._
+  assert(implicitly[Extractor[Int :: String :: HNil]].extract == 1 :: "b" :: HNil)
+  assert(implicitly[Extractor[(String, Int)]].extract == ("a", 2))
+  assert(implicitly[Extractor[(Int, Double, String, Double, String, Int)]].extract == (1, 2.0, "c", 4.0, "e", 6))
 
 //  Class.forName("org.h2.Driver")
 //  val conn = DriverManager.getConnection("jdbc:h2:mem:;MODE=MYSQL")
