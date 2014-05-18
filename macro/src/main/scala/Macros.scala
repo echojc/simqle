4b456d08ea7e290ed47502192e0d8224c2143420
Jonathan Chow <me@echo.sh>
2014-05-18 21:46:58 +1200

    Ported over to RowMapper/ColumnMapper traits.

--- macro/src/main/scala/Macros.scala
+++ macro/src/main/scala/Macros.scala
@@ -33,9 +33,63 @@ trait RowMapper[T] {
   def fromResultSet(rs: ResultSet): Option[T]
 }
 
-object RowMapper {
+object RowMapper extends RowMapperLowerPriorityImplicits {
 
+  import nat._1
+  import ops.nat.ToInt
+  trait HListRowMapper[L <: HList, N <: Nat] extends RowMapper[L] {
+    type H
+    type T <: HList
+  }
+  implicit def hnilRowMapper[N <: Nat] = new HListRowMapper[HNil, N] {
+    def fromResultSet(rs: ResultSet): Option[HNil] = Some(HNil)
+  }
+  object HListRowMapper {
+    def apply[L <: HList, N <: Nat](implicit hrm: HListRowMapper[L, N]): Aux[L, hrm.H, hrm.T, N] = hrm
+    type Aux[L <: HList, H0, T0 <: HList, N <: Nat] = HListRowMapper[L, N] { type H = H0; type T = T0 }
+    implicit def hlistRowMapper[H0, T0 <: HList, N <: Nat](
+      implicit hx: ColumnMapper[H0], tx: HListRowMapper[T0, Succ[N]], toInt: ToInt[N]): Aux[H0 :: T0, H0, T0, N] =
+      new HListRowMapper[H0 :: T0, N] {
+        type H = H0
+        type T = T0
+        def fromResultSet(rs: ResultSet): Option[H0 :: T0] = {
+          val index = toInt()
+          if (index == 1 && !rs.next()) None
+          else tx.fromResultSet(rs) map (hx.fromResultSet(rs, index) :: _)
+        }
+      }
+  }
+
+  import ops.hlist.Tupler
+  implicit def tupleRowMapper[P, L <: HList](
+    implicit gen: Generic.Aux[P, L], hx: HListRowMapper[L, _1], tp: Tupler.Aux[L, P]) =
+    new RowMapper[P] {
+      def fromResultSet(rs: ResultSet): Option[P] = hx.fromResultSet(rs) map (tp.apply)
+    }
+
+  implicit object IntRowMapper extends RowMapper[Int] {
+    val columnMapper = implicitly[ColumnMapper[Int]]
+    def fromResultSet(rs: ResultSet): Option[Int] = {
+      if (!rs.next()) None
+      else Option(columnMapper.fromResultSet(rs, 1))
+    }
+  }
+
+  implicit object StringRowMapper extends RowMapper[String] {
+    val columnMapper = implicitly[ColumnMapper[String]]
+    def fromResultSet(rs: ResultSet): Option[String] = {
+      if (!rs.next()) None
+      else Option(columnMapper.fromResultSet(rs, 1))
+    }
+  }
+}
+
+trait RowMapperLowerPriorityImplicits {
+  import RowMapperLowerPriorityImplicits._
   implicit def caseClassRowMapper[T]: RowMapper[T] = macro materializeCaseClassRowMapper[T]
+}
+
+object RowMapperLowerPriorityImplicits {
 
   def materializeCaseClassRowMapper[T: c.WeakTypeTag](c: Context): c.Expr[RowMapper[T]] = {
     import c.universe._
@@ -62,35 +116,6 @@ object RowMapper {
       }
     """ }
   }
-
-//  implicit def tuple2RowMapper[A: ColumnMapper, B: ColumnMapper]: RowMapper[(A, B)] =
-//    new RowMapper[(A, B)] {
-//      val columnMapperA = implicitly[ColumnMapper[A]]
-//      val columnMapperB = implicitly[ColumnMapper[B]]
-//      def fromResultSet(rs: ResultSet): Option[(A, B)] = {
-//        if (!rs.next()) None
-//        else Option((
-//          columnMapperA.fromResultSet(rs, 1),
-//          columnMapperB.fromResultSet(rs, 2)
-//        ))
-//      }
-//    }
-
-  implicit object IntRowMapper extends RowMapper[Int] {
-    val columnMapper = implicitly[ColumnMapper[Int]]
-    def fromResultSet(rs: ResultSet): Option[Int] = {
-      if (!rs.next()) None
-      else Option(columnMapper.fromResultSet(rs, 1))
-    }
-  }
-
-  implicit object StringRowMapper extends RowMapper[String] {
-    val columnMapper = implicitly[ColumnMapper[String]]
-    def fromResultSet(rs: ResultSet): Option[String] = {
-      if (!rs.next()) None
-      else Option(columnMapper.fromResultSet(rs, 1))
-    }
-  }
 }
 
 object Sql {
@@ -98,6 +123,9 @@ object Sql {
   def one[T](rs: ResultSet)(implicit m: RowMapper[T]): Option[T] =
     m.fromResultSet(rs)
 
-  def seq[T](rs: ResultSet)(implicit m: RowMapper[T]): Seq[T] =
+  def stream[T](rs: ResultSet)(implicit m: RowMapper[T]): Stream[T] =
     (Stream.continually(one(rs)) takeWhile (_.nonEmpty)).flatten
+
+  def list[T](rs: ResultSet)(implicit m: RowMapper[T]): List[T] =
+    stream(rs).toList
 }
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,91 +1,32 @@
 import java.sql.DriverManager
 import scala.collection.mutable
 
-import shapeless._
-
-
-object A {
-
-  trait Extractor[T] { def extract: T }
-  implicit object intExtractor extends Extractor[Int] { def extract = 1 }
-  implicit object stringExtractor extends Extractor[String] { def extract = "a" }
-
-  trait NE[T] { def extract(i: Int): T }
-  implicit object intNExtractor extends NE[Int] { def extract(i: Int) = i }
-  implicit object stringNExtractor extends NE[String] { def extract(i: Int) = ('a' + i - 1).toChar.toString }
-  implicit object doubleNExtractor extends NE[Double] { def extract(i: Int) = 1.0 * i }
-
-  import nat._
-  import ops.nat._
-  trait LowerPriority {
-    implicit def hlistExtractor[L <: HList](implicit hlnx: HListNExtractor[L, _2]): HListNExtractor[L, _2] = hlnx
-  }
-
-  trait HListNExtractor[L <: HList, N <: Nat] extends Extractor[L] {
-    type H
-    type T <: HList
-  }
-  implicit def hnilNExtractor[N <: Nat] = new HListNExtractor[HNil, N] { def extract = HNil }
-  object HListNExtractor extends LowerPriority {
-    def apply[L <: HList, N <: Nat](implicit hlx: HListNExtractor[L, N]): Aux[L, hlx.H, hlx.T, N] = hlx
-
-    type Aux[L <: HList, H0, T0 <: HList, N <: Nat] = HListNExtractor[L, N] { type H = H0; type T = T0 }
-
-    implicit def auxExtractor[H0, T0 <: HList, N <: Nat](
-      implicit hx: NE[H0], tx: HListNExtractor[T0, Succ[N]], toInt: ToInt[N]): Aux[H0 :: T0, H0, T0, N] =
-      new HListNExtractor[H0 :: T0, N] {
-        type H = H0
-        type T = T0
-        def extract: H0 :: T0 = hx.extract(toInt()) :: tx.extract
-      }
-  }
-
-  import ops.hlist.Tupler
-  implicit def tupleExtractor[P, L <: HList](
-    implicit gen: Generic.Aux[P, L], hx: HListNExtractor[L, _1], tp: Tupler.Aux[L, P]) =
-    new Extractor[P] {
-      def extract: P = tp(hx.extract)
-    }
-}
-
-
 object Main extends App {
-  import A._
-  import A.HListNExtractor._
-  assert(implicitly[Extractor[Int :: String :: HNil]].extract == 1 :: "b" :: HNil)
-  assert(implicitly[Extractor[(String, Int)]].extract == ("a", 2))
-  assert(implicitly[Extractor[(Int, Double, String, Double, String, Int)]].extract == (1, 2.0, "c", 4.0, "e", 6))
 
-//  Class.forName("org.h2.Driver")
-//  val conn = DriverManager.getConnection("jdbc:h2:mem:;MODE=MYSQL")
-//
-//  val stmt = conn.createStatement()
-//  stmt.execute("create table foo (a int, b varchar(255))")
-//  stmt.execute("insert into foo (a, b) values (1, 'a'), (2, 'b')")
-//
-//  val rs1 = stmt.executeQuery("select b from foo")
-//  println(Sql.seq[String](rs1).toList)
-//
-//  val rs2 = stmt.executeQuery("select a from foo")
-//  println(Sql.one[Int](rs2))
-//
-////  val rs3 = stmt.executeQuery("select a, b from foo")
-////  println(Sql.seq[(Int, String)](rs3).toList)
-//
-//  case class Foo(a: Int, b: String)
-//  val rs4 = stmt.executeQuery("select * from foo")
-//  println(Sql.seq[Foo](rs4).toList)
-//
-//  case class Bar(b: String)
-//  val rs5 = stmt.executeQuery("select * from foo")
-//  println(Sql.seq[Bar](rs5).toList)
-//
-////  while (rs.next()) {
-////    val a = rs.getInt("a")
-////    val b = rs.getString("b")
-////    println(Foo(a, b))
-////  }
-//
-//  stmt.close()
-//  conn.close()
+  Class.forName("org.h2.Driver")
+  val conn = DriverManager.getConnection("jdbc:h2:mem:;MODE=MYSQL")
+
+  val stmt = conn.createStatement()
+  stmt.execute("create table foo (a int, b varchar(255))")
+  stmt.execute("insert into foo (a, b) values (1, 'a'), (2, 'b')")
+
+  val rs1 = stmt.executeQuery("select b from foo")
+  assert(Sql.list[String](rs1) == List("a", "b"))
+
+  val rs2 = stmt.executeQuery("select a from foo")
+  assert(Sql.one[Int](rs2) == Option(1))
+
+  val rs3 = stmt.executeQuery("select a, b from foo")
+  assert(Sql.list[(Int, String)](rs3) == List((1, "a"), (2, "b")))
+
+  case class Foo(a: Int, b: String)
+  val rs4 = stmt.executeQuery("select * from foo")
+  assert(Sql.list[Foo](rs4) == List(Foo(1, "a"), Foo(2, "b")))
+
+  case class Bar(b: String)
+  val rs5 = stmt.executeQuery("select * from foo")
+  assert(Sql.list[Bar](rs5) == List(Bar("a"), Bar("b")))
+
+  stmt.close()
+  conn.close()
 }
